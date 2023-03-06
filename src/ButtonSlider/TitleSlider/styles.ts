8e13bc5802a2ef1a3fa03f69a3a816db6ae83e7c
Supra0km <miguelcastroferreira5@gmail.com>
2023-03-06 12:21:10 -0300

    mobile

---
+++ src/ButtonSlider/TitleSlider/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    tittle: {
+        fontSize: 40,
+        marginTop: 20,
+        margin: 10,
+        textAlign: 'center'
+    },
+})
---
+++ src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    primary: '#2b75E2',
+    secondary: '#FFC107',
+    third: '#D9D9D9',
+    thirdLight: '#A3B7',
+    black: '#000',
+    white: '#FFF'
+}
