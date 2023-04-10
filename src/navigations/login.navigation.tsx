0cb188a463109998cfa8c36f9fe7e11c709dfb97
Supra0km <miguelcastroferreira5@gmail.com>
2023-04-10 12:28:11 -0300

    Sucesso

--- src/navigations/login.navigation.tsx
+++ src/navigations/login.navigation.tsx
@@ -1,15 +1,23 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
 import {ScreenCadastrar, ScreenLogin} from "../screens"
+type LoginStackParamList = {
+  Login: undefined
+  Cadastrar: undefined
+};
+type LoginScreenNavigation = StackNavigationProp<LoginStackParamList, "Login" >
+export type LoginTypes = {
+  navigation: LoginScreenNavigation
+}
 
-const Stack = createStackNavigator();
 
 export function LoginNavigation() {
+  const Stack = createStackNavigator<LoginStackParamList>();
   return (
     <Stack.Navigator>
 
       <Stack.Screen name="Login" component={ScreenLogin} />
-      <Stack.Screen name="Cadastre-se" component={ScreenCadastrar} />
+      <Stack.Screen name="Cadastrar" component={ScreenCadastrar} />
 
     </Stack.Navigator>
   );
