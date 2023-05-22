7f5a0ffad4fcf0746a0f7d212ca3d2e0caf2ec43
Supra0km <miguelcastroferreira5@gmail.com>
2023-05-22 12:19:51 -0300

    isso

--- src/screens/Camera/index.tsx
+++ src/screens/Camera/index.tsx
@@ -1,8 +1,11 @@
-import { Camera, CameraType } from 'expo-camera';
+import { Camera, CameraCapturedPicture, CameraType } from 'expo-camera';
 import { useRef, useState } from 'react';
-import { Button, Text, TouchableOpacity, View, Image } from 'react-native';
+import { Button, Text, TouchableOpacity, View, Image, Alert } from 'react-native';
 import { ComponentButtonInterface } from '../../components';
 import { styles } from "./styles"
+import * as MediaLibrary from 'expo-media-library'
+import * as ImagePicker from 'expo-image-picker';
+
 interface iPhoto {
   height: string
   uri: string
@@ -11,24 +14,42 @@ interface iPhoto {
 
 export function CameraScreen() {
   const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
-  const [photo, setPhoto] = useState<iPhoto>()
-  const ref = useRef(null)
+  const [permissionCamera, requestPermissionCamera] = Camera.useCameraPermissions();
+  const [permissionMedia, requestPermissionMedia] = MediaLibrary.usePermissions()
+  const [photo, setPhoto] = useState<CameraCapturedPicture | ImagePicker.ImagePickerAsset>()
+  const ref = useRef<Camera>(null)
+  const [takePhoto, setTakePhoto] = useState(false)
 
-  if (!permission) {
+  if (!permissionCamera) {
     // Camera permissions are still loading
     return <View />;
   }
 
-  if (!permission.granted) {
+  if (!permissionCamera.granted) {
     // Camera permissions are not granted yet
     return (
       <View style={styles.container}>
         <Text style={{ textAlign: 'center' }}>Por sua conta e risco, permita a câmera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+        <Button onPress={requestPermissionCamera} title="grant permission" />
       </View>
     );
   }
+
+  if (!permissionMedia) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permissionMedia.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>Por sua conta e risco, permita a galeria</Text>
+        <Button onPress={requestPermissionMedia} title="grant permission" />
+      </View>
+    );
+  }
+
 
   function toggleCameraType() {
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
@@ -42,10 +63,29 @@ export function CameraScreen() {
     }
   }
 
+  async function savePhoto() {
+    const asset = await MediaLibrary.createAssetAsync(photo!.uri)
+    MediaLibrary.createAlbumAsync("Images", asset, false)
+    Alert.alert("Imagem salva com sucesso!")
+  }
+
+  async function pickImage() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    })
+    if (!result.canceled){
+      setPhoto(result.assets[0])
+    }
+  }
+
   return (
     <View style={styles.container}>
         <Camera style={styles.camera} type={type} ref={ref}/>
         <ComponentButtonInterface title='Flip' type='secondary' onPressI={toggleCameraType} />
+        <ComponentButtonInterface title='Salvar' type='secondary' onPressI={savePhoto} />
+        <ComponentButtonInterface title='Abrir imagem' type='secondary' onPressI={pickImage} />
         <ComponentButtonInterface title='Tirar foto' type='secondary' onPressI={takePicture} />
         {photo && photo.uri && (
           <Image source={{uri:photo.uri }} style={styles.img} />
