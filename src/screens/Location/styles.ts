d09784976f6554453d6f1f3eb6828aa8f5c86b04
Supra0km <miguelcastroferreira5@gmail.com>
2023-06-19 12:12:57 -0300

    1906

--- src/screens/Location/styles.ts
+++ src/screens/Location/styles.ts
@@ -1,6 +1,8 @@
-import { Dimensions, StyleSheet } from "react-native";
-
-export const styles = StyleSheet.create({
+import {Dimensions, StyleSheet} from 'react-native'
+import { colors } from '../../styles/colors';
+ 
+ 
+ export const styles = StyleSheet.create({
     container: {
       flex: 1,
       alignItems: 'center',
@@ -11,8 +13,22 @@ export const styles = StyleSheet.create({
       fontSize: 18,
       textAlign: 'center',
     },
-    map: {
-      width: Dimensions.get("window").width,
+    map:{
+      widht: Dimensions.get("window").width,
       height: Dimensions.get("window").height
+    },
+    searchContainer:{
+      position: 'absolute',
+      zIndex: 1,
+      width: '90%' ,
+      top: 10
+
+    },
+    searchInput:{
+      height: 56,
+      borderRadius: 7,
+      borderWidth: 2,
+      borderColor: colors.third
     }
   });
+  
